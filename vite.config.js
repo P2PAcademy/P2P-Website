6d9046170d4541887ad2bb99111f87e871bf9ffa
user <user@kali>
2025-06-13 23:20:25 +0500

    Fix MIME error and update build output

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/P2P-Website/",
+  base: "/P2P-Website/", // This should match your repository name
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
