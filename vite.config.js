92556bbd36fd108a52fa711e3b5e6bc1006d082a
user <user@kali>
2025-06-13 22:55:23 +0500

    Update build files and config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/P2P-Website/",
+  base: './',
   plugins: [react(), tailwindcss()],
 })
